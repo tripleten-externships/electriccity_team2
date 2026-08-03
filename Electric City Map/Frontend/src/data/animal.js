09eb83188aeb357e8c5aab5f0f427c4a492b975a
Brandi McDill <mcdillbrandig@gmail.com>
2026-08-03 15:51:42 -0400

    chore: add initial data template structure

---
+++ Electric City Map/Frontend/src/data/animal.js
@@ -0,0 +1,13 @@
+export const animals = [
+    {
+        id: ,
+        image: ,
+        name: ,
+        status: ,
+        native_region: ,
+        taxonomy_category: ,
+        threats: ,
+        fun_fact: ,
+        coordinates: ,
+    },
+]
