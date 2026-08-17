83513511b71e7be9bf78509cca949efdd13581c6
Brandi McDill <mcdillbrandig@gmail.com>
2026-08-17 17:28:34 -0400

    fix(deploy): coordinate asset subfolder paths between router and vite build configs

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './components/App/App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/electriccity_team2">
+    <BrowserRouter basename="/electriccity_team2/">
     <App />
     </BrowserRouter>
   </StrictMode>,
