9a8987dcbc2f96f7d827a57ce43965bd5ee5be6b
Brandi McDill <mcdillbrandig@gmail.com>
2026-08-17 17:07:26 -0400

    chore: remove unnecessary folders

---
+++ src/components/App/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import { Routes, Route, useNavigate, useLocation } from 'react-router-dom';
+import LandingPage from '../../components/LandingPage/LandingPage';
+import WorldMap from '../../components/WorldMap/WorldMap';
+
+import './App.css'
+
+function App() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === '/') return;
+
+    const timer = setTimeout (() => {
+      navigate('/');
+    }, 120000);
+
+    const resetTimer =() => {
+      clearTimeout(timer);
+      timer=setTimeout(() => {
+        navigate('/');
+      }, 120000);
+    };
+    window.addEventListener('touchstart', resetTimer);
+    
+    return () => {
+      clearTimeout(timer);
+      window.removeEventListener('touchstart', resetTimer);
+    };
+  }, [location, navigate]);
+
+  return (
+   <div className="app">
+    <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/map" element={<WorldMap key="world-map" />} />
+      </Routes>
+    </div>
+  )
+};
+
+export default App
